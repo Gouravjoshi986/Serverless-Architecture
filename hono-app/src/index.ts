a0df90d907dfe3aa1b91a88bb05172df3f7c9315
Gouravjoshi986 <gourav.joshi2003@gmail.com>
2024-05-05 08:48:06 +0530

    added midlewares in hono

--- hono-app/src/index.ts
+++ hono-app/src/index.ts
@@ -2,6 +2,17 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+async function authMiddleware(c:any,next:any){
+  if(c.req.header('Authorization')){
+    // authorization checks 
+    await next();
+  }
+  else {
+    return c.text("You dont have the access")
+  }
+}
+app.use(authMiddleware);
+
 app.get('/',async (c) => {
   // For Inputs    - body,query parameter,header,middleware,db connection
   const body = await c.req.json();
